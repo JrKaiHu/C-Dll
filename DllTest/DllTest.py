5b0fa8cb83ba31794ec45dc285b257b7754c6eff
JrKaiHu <JrKaiHu@users.noreply.github.com>
2017-06-29 00:47:40 +0800

    Python Dll Test
    1. Add Python Dll test.

---
+++ DllTest/DllTest.py
@@ -0,0 +1,36 @@
+from ctypes import *
+
+import ctypes
+
+class SUB_PARA1(Structure):
+    _fields_ = [ ("nPara1", c_int),
+                 ("nPara2", c_int)]
+
+class SUB_PARA2(Structure):
+    _fields_ = [ ("nPara3", c_int),
+                 ("nPara4", c_int)]
+
+class SUB_PARA3(Structure):
+    _fields_ = [ ("nPara5", c_int),
+                 ("nPara6", c_int)]
+
+class PROFILE(Structure):
+    _fields_ = [("szName", c_char * 20),
+                ("szAge", c_char * 20)]
+
+class PARAMETER(Structure):
+    _fields_ = [ ("SUB_PARA1", SUB_PARA1),
+                 ("SUB_PARA2", SUB_PARA2),
+                 ("SUB_PARA3", SUB_PARA3),
+                 ("PROFILE", PROFILE)]
+
+dll = CDLL("MathDll.dll")
+para = PARAMETER()
+para.SUB_PARA1.nPara1 = 5
+para.SUB_PARA1.nPara2 = 1
+para.SUB_PARA2.nPara3 = 20
+para.SUB_PARA2.nPara4 = 17
+para.SUB_PARA3.nPara5 = 3
+para.SUB_PARA3.nPara6 = 9
+
+print(dll.Add(byref(para)))
